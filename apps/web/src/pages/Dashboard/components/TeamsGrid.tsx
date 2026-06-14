f1ed4145a41b56cc6fda8e5dcdfa59a5a79d100a
AdrianRangel36 <adrianrvdev@gmail.com>
2026-06-14 16:24:43 -0600

    feat: add handleaccept invite function

--- apps/web/src/pages/Dashboard/components/TeamsGrid.tsx
+++ apps/web/src/pages/Dashboard/components/TeamsGrid.tsx
@@ -6,35 +6,47 @@ import { LoadingState } from "./LoadingState";
 import type { Team } from "@/interfaces";
 import { InviteCard } from "./InviteCard";
 import { useNavigate } from "react-router-dom";
+import { useDashboard } from "../hooks";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 interface TeamsGridProps {
   teams: Team[];
   isLoading: boolean;
-  onTeamClick: (teamId: number) => void;
   onCreateTeam: (teamName: string) => Promise<void>;
   isDialogOpen: boolean;
   onDialogOpenChange: (open: boolean) => void;
 }
 
 export const TeamsGrid: React.FC<TeamsGridProps> = ({
-  teams,
   isLoading,
-  onTeamClick,
   onCreateTeam,
   isDialogOpen,
   onDialogOpenChange,
 }) => {
   const navigate = useNavigate();
-
+  const { teams, user, fetchTeams } = useDashboard();
   const handleNavigateToKanban = (teamId: number) => {
     navigate(`/team/${teamId}`);
   };
 
   const handleAcceptInvite = async (teamId: number) => {
+    const token = localStorage.getItem("token");
     try {
-      // API call a NestJS: api.patch(`/team/${teamId}/members/accept`);
-      console.log("Aceptando invitación al equipo", teamId);
-      // if (onRefetchTeams) onRefetchTeams(); // Refrescar vista
+      const response = await fetch(`${API_BASE_URL}/team-members/changerole`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          userId: user?.id,
+          teamId: teamId,
+          role: "MEMBER",
+        }),
+      });
+
+      if (!response.ok) throw Error("Error al aceptar la invitación");
+      fetchTeams();
     } catch (error) {
       console.error("Error al aceptar", error);
     }
@@ -48,7 +60,6 @@ export const TeamsGrid: React.FC<TeamsGridProps> = ({
       console.error("Error al rechazar", error);
     }
   };
-  console.log(teams);
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
       <CreateTeamDialog
@@ -62,13 +73,13 @@ export const TeamsGrid: React.FC<TeamsGridProps> = ({
       ) : teams.length === 0 ? (
         <EmptyState />
       ) : (
-        teams.map((team) => {          
+        teams.map((team) => {
           if (team.role === "PENDING") {
             return (
               <InviteCard
                 key={`invite-${team.teamId}`}
                 team={team}
-                onAccept={handleAcceptInvite}
+                onAccept={() => handleAcceptInvite(team.teamId)}
                 onReject={handleRejectInvite}
               />
             );
